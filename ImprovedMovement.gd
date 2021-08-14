fe43a387fc359e1ca587e7db9d0ee8774ca005e2
lcourbi <luc.courbariaux@gmail.com>
2021-08-15 00:49:03 +0200

    add warning and workaround for bug when setting translation driectly

--- ImprovedMovement.gd
+++ ImprovedMovement.gd
@@ -1,4 +1,5 @@
 extends KinematicBody
+tool
 
 class_name ImprovedKinematicBody
 
@@ -19,7 +20,7 @@ func _physics_process(delta):
 	_physicsTranslation = self.translation
 	_physicsUpdate = _getTime()
 
-func _process(delta):
+func _process(_delta):
 	var now = _getTime()
 	var diff =  now - _physicsUpdate
 	if _physicsTranslation != Vector3.ZERO and diff > 0:
@@ -29,3 +30,16 @@ func _implementMovement(delta):
 	# implement movement here. Ex:
 	#velocity = move_and_slide_with_snap(velocity, Vector3.DOWN, Vector3.UP, true, 1, deg2rad(45), false)
 	pass
+
+#############
+# workaround for bug :
+# when you set this node's translation, it reverts back because of _physicsTranslation still being the old position
+# I would have liked to override _set_transform and _set_translation accordingly, but it isn't possible from GDscript
+func changeTransform(new_transform):
+	_physicsUpdate = _getTime()
+	_physicsTranslation = new_transform.origin
+	self.transform = new_transform
+
+# to show warning about teleport
+func _get_configuration_warning():
+	return "use changeTransform(new_transform) when teleporting this node !"
--- README.md
+++ README.md
@@ -8,12 +8,15 @@ This version :
 * doesn't lag behind the physics model
 * uses extrapolation rather than interpolation
 * as such, needs a velocity vector
+* requires special care when teleporting it around (see 'How to use')
     
 ### How to use
 - Add [ImprovedMovement.gd](https://github.com/Lcbx/Godot_SmoothMovement/blob/master/ImprovedMovement.gd "link to script")   to the project files  
 - change the type of existing _KinematicBody_ to _ImprovedKinematicBody_
 - modify scripts to contain _'extends ImprovedKinematicBody'_ 
 - modify scripts to implement the function **_implementMovement(delta)** as if it was **_physics_process**
+- use the **velocity** variable to control it's movement the way you would a _RigidBody_
+- call _changeTransform(newTransform)_ when changing the Node's translation directly
 
 You can use the demo scene as a template.
 
